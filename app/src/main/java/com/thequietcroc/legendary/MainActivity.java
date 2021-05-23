92cc157cea3ada206576821730a6d88d14b9decb
Samuel Marino <TheQuietCroc@gmail.com>
2021-05-23 16:35:36 -0500

    Added randomization

--- app/src/main/java/com/thequietcroc/legendary/MainActivity.java
+++ app/src/main/java/com/thequietcroc/legendary/MainActivity.java
@@ -1,6 +1,9 @@
 package com.thequietcroc.legendary;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
@@ -11,7 +14,12 @@ import com.thequietcroc.legendary.custom.views.CardControl;
 import com.thequietcroc.legendary.database.LegendaryDatabase;
 import com.thequietcroc.legendary.enums.CardType;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Random;
+import java.util.Set;
 
 import static com.thequietcroc.legendary.enums.CardType.HENCHMEN;
 import static com.thequietcroc.legendary.enums.CardType.HERO;
@@ -21,8 +29,6 @@ import static com.thequietcroc.legendary.enums.CardType.VILLAINS;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final String EXTRA_MESSAGE = "com.thequietcroc.legendary.MESSAGE";
-
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,6 +37,50 @@ public class MainActivity extends AppCompatActivity {
         final LegendaryDatabase db = LegendaryDatabase.getInstance(this);
 
         initializeControls(db);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(final Menu menu) {
+        final MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_activity_menu_actions, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(final MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menuActionRandomize: {
+
+                final List<CardControl> cardControls = Arrays.asList(new CardControl[]{
+                        findViewById(R.id.cardControlMastermind),
+                        findViewById(R.id.cardControlScheme),
+                        findViewById(R.id.cardControlVillains1),
+                        findViewById(R.id.cardControlVillains2),
+                        findViewById(R.id.cardControlVillains3),
+                        findViewById(R.id.cardControlVillains4),
+                        findViewById(R.id.cardControlHenchmen1),
+                        findViewById(R.id.cardControlHenchmen2),
+                        findViewById(R.id.cardControlHeroes1),
+                        findViewById(R.id.cardControlHeroes2),
+                        findViewById(R.id.cardControlHeroes3),
+                        findViewById(R.id.cardControlHeroes4),
+                        findViewById(R.id.cardControlHeroes5),
+                        findViewById(R.id.cardControlHeroes6)
+                });
+
+                randomizeSelections(Collections.singletonList(cardControls.get(0)));
+                randomizeSelections(Collections.singletonList(cardControls.get(1)));
+                randomizeSelections(cardControls.subList(2, 6));
+                randomizeSelections(cardControls.subList(6, 8));
+                randomizeSelections(cardControls.subList(8, 14));
+            }
+            break;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+        return true;
     }
 
     private void initializeControls(final LegendaryDatabase db) {
@@ -103,6 +153,25 @@ public class MainActivity extends AppCompatActivity {
             adapter.setDropDownViewResource(R.layout.spinner_layout);
             spinner.setAdapter(adapter);
         };
+    }
+
+    private void randomizeSelections(final List<CardControl> cardControls) {
+        final Set<Integer> results = new HashSet<>(cardControls.size());
+
+        for (final CardControl cardControl : cardControls) {
+            if (!cardControl.getToggleLock().isChecked()) {
+
+                final Spinner spinner = cardControl.getSpinner();
+                final int numCards = spinner.getCount();
+                int selection;
+
+                do {
+                    selection = new Random().nextInt(numCards - 1) + 1;
+                } while (!results.add(selection));
+
+                spinner.setSelection(selection);
+            }
+        }
     }
 }
 
